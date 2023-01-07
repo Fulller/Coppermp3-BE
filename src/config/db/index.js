9c96dc95ea7de2d48fa31f1df4a8ba2c03c825d6
CopperFull <nguyenday3332@gmail.com>
2023-01-07 15:29:52 +0700

    done

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/coppermp3", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://zingmp3Username:zingmp3Password@zingmp3cluster.esie2lt.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("Connect successfully <3");
   } catch (e) {
     console.log("Connect faile!!!");
